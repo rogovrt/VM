bc3b1e218f8c330906ab962fd1e50f9553c617e5
rogovrt <49844050+rogovrt@users.noreply.github.com>
2021-02-09 22:08:32 +0300

    Changes in transfer

--- transfer/main.cpp
+++ transfer/main.cpp
@@ -5,11 +5,10 @@
 int main() {
     std::ofstream fout("out.txt");
     fout.setf(std::ios::fixed);
-    fout.precision(1);
+    fout.precision(4);
     std::array <std::array<long double, 201>, 2001> grid{};
     long double t = .005;
     long double h = .01;
-
 
     for (int i = 0; i < 201; ++i)
         grid[0][i] = 0.;
@@ -18,33 +17,26 @@ int main() {
 
     for (int i = 1; i < 2001; ++i) {
         for (int j = 1; j < 201; ++j)
-            grid[i][j] = (1 + 2 * (grid[i-1][j]-grid[i-1][j-1])/h)*t + grid[i-1][j];
-    }
-    /*for (int i = 1; i < 2001; ++i) {
-        for (int j = 99; j > -1; --j)
             grid[i][j] = (1 + 2 * (grid[i-1][j+1]-grid[i-1][j])/h)*t + grid[i-1][j];
-    }*/
+    }
 
     fout << "t = 0.1\n";
     for (int j = 0; j < 201; ++j) {
-        if (j % 10 == 0)
-            fout << grid[20][j] << " ; ";
+        fout << grid[20][j] << " , ";
     }
     fout << std::endl;
     fout << std::endl;
 
     fout << "t = 1\n";
     for (int j = 0; j < 201; ++j) {
-        if (j % 10 == 0)
-            fout << grid[200][j] << " ; ";
+        fout << grid[200][j] << " , ";
     }
     fout << std::endl;
     fout << std::endl;
 
     fout << "t = 10\n";
     for (int j = 0; j < 201; ++j) {
-        if (j % 10 == 0)
-            fout << grid[2000][j] << " ; ";
+        fout << grid[2000][j] << " , ";
     }
 
     return 0;
